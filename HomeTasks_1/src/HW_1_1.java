47b98dde2e4f1015573fb41ef4e3454aeef06d7e
M-Ostapinskiyy <ostapinskiyy@gmail.com>
2025-08-16 09:38:04 +0300

    Home Task 1 v.2.

---
+++ HomeTasks_1/src/HW_1_1.java
@@ -0,0 +1,12 @@
+public class HW_1_1 {
+    public static void main(String[] args) {
+
+        /** Задача №1 */
+
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.print("Поехали!!!");
+
+    }
+}
