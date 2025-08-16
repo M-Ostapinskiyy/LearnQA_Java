596f85373e05023541dfb2f4fded6915aa52803a
M-Ostapinskiyy <ostapinskiyy@gmail.com>
2025-08-16 09:37:41 +0300

    Home Task 1 v.2

---
+++ HomeTasks_1/src/HW_1_3.java
@@ -0,0 +1,11 @@
+public class HW_1_3 {
+    public static void main(String[] args) {
+
+        /** Экстра задача */
+        /** Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом) */
+
+        String emoji = "\ud83d\ude42";
+        System.out.println(emoji);
+
+    }
+}
